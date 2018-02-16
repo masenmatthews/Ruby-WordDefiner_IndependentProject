0ca4d550b0f01e49e7a99cba94ca16b6bb78f870
Masen Matthews <masenmatthews@gmail.com>
2018-02-16 14:30:21 -0800

    trim unneccesary code, refactor

--- app.rb
+++ app.rb
@@ -1,10 +1,10 @@
 require('sinatra')
 require('sinatra/reloader')
-require('rspec')
-require('pry')
 also_reload('lib/**/*.rb')
 require('./lib/definition_class')
 require('./lib/word_class')
+require('rspec')
+require('pry')
 
 get('/') do
   @word_list = Word.list
@@ -12,10 +12,23 @@ get('/') do
 end
 
 post('/') do
-  word = params["word"]
+  word = params["word_input"]
   word_value = Word.new({:term => word})
   word_value.populate_list()
   list_of_words = Word.list
   @word_list = list_of_words
   erb(:word_input)
+end
+
+get('/word_input/:term') do
+  @word_list = []
+  @word_value = params.fetch(:term)
+  list_of_definitions = Word.list
+  @definition_list = list_of_definitions
+  erb(:definition_input)
+end
+
+post('/clear') do
+  Word.clear
+  redirect '/'
 end
--- spec/word_definer_integration_spec.rb
+++ spec/word_definer_integration_spec.rb
@@ -1,14 +1,13 @@
-# require('capybara/rspec')
-# require('./app')
-# Capybara.app = Sinatra::Application
-# set(:show_exceptions, false)
-#
-# describe('the rectangle path', {:type => :feature}) do
-#   it('processes the user entry and returns whether it is a square or not') do
-#     visit('/')
-#     fill_in('length', :with => '5')
-#     fill_in('width', :with => '5')
-#     click_button('Go!')
-#     expect(page).to have_content('This is a square.')
-#   end
-# end
+require('capybara/rspec')
+require('./app')
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+describe('word is added to list when add button is clicked', {:type => :feature}) do
+  it('adds input text to list') do
+    visit('/')
+    fill_in('word_input', :with => 'dog')
+    click_button('Add word')
+    expect(page).to have_content('dog')
+  end
+end
