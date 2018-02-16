d4198392cd2367a6162ae9a2ac74ca7704914710
Masen Matthews <masenmatthews@gmail.com>
2018-02-16 13:05:38 -0800

    bug fix - list population

--- app.rb
+++ app.rb
@@ -7,26 +7,15 @@ require('./lib/definition_class')
 require('./lib/word_class')
 
 get('/') do
-  list_of_words = Word.list
+  @word_list = Word.list
   erb(:word_input)
 end
 
 post('/') do
-  word = params["word_input"]
+  word = params["word"]
   word_value = Word.new({:term => word})
   word_value.populate_list()
   list_of_words = Word.list
   @word_list = list_of_words
   erb(:word_input)
-end
-
-# get('/word/:word') do
-#   @word_list = []
-#   @inputted_word = params.fetch("word_input")
-#   erb(:definition_input)
-# end
-
-post('/word/defioitions') do
-  album = params("album_input")
-  erb(:definition_input)
 end
