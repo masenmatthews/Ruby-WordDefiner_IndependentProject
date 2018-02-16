79507b27d0abaccd53ea102b9e2de861e7da4350
Masen Matthews <masenmatthews@gmail.com>
2018-02-16 11:08:59 -0800

    expand front-end in app.rb

--- app.rb
+++ app.rb
@@ -1,20 +1,32 @@
 require('sinatra')
 require('sinatra/reloader')
+require('rspec')
+require('pry')
 also_reload('lib/**/*.rb')
 require('./lib/definition_class')
 require('./lib/word_class')
-require('rspec')
-require('pry')
 
 get('/') do
   list_of_words = Word.list
+  erb(:word_input)
 end
 
 post('/') do
-  word = params("definition_input")
+  word = params("word_input")
   word_value = Word.new({:term => word})
   word_value.populate_list()
   Word.sort_list
   list_of_words = Word.list
+  @word_list = list_of_words
   erb(:word_input)
+end
+
+get('/word/:word') do
+  @word_list = []
+  @inputted_word = params.fetch("word_input")
+  erb(:definition_input)
+end
+
+post('/word/defioitions') do
+  
 end
