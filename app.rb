dbb91a9c9da8bc0889e7bef1c4cc9999289d3b8e
Masen Matthews <masenmatthews@gmail.com>
2018-02-16 10:47:12 -0800

    build out app.rb

--- app.rb
+++ app.rb
@@ -5,3 +5,16 @@ require('./lib/definition_class')
 require('./lib/word_class')
 require('rspec')
 require('pry')
+
+get('/') do
+  list_of_words = Word.list
+end
+
+post('/') do
+  word = params("definition_input")
+  word_value = Word.new({:term => word})
+  word_value.populate_list()
+  Word.sort_list
+  list_of_words = Word.list
+  erb(:word_input)
+end
