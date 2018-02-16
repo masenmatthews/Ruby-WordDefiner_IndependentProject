8b8dadc74eb2324b9b15dc13cd70d9dffdd52da5
Masen Matthews <masenmatthews@gmail.com>
2018-02-16 10:10:05 -0800

    Third spec added, failing.

--- spec/word_definer_spec.rb
+++ spec/word_definer_spec.rb
@@ -5,17 +5,25 @@ require('pry')
 
 describe('Word') do
   describe('#initalize') do
-    it('initializes new object under Word class') do
+      it('initializes new object under Word class') do
+        word = Word.new({:term => 'Dog'})
+        expect(word.term).to(eq('Dog'))
+      end
+    end
+
+  describe('#populate_list') do
+    it('populates list with inputted term') do
       word = Word.new({:term => 'Dog'})
-      expect(word.term).to(eq('Dog'))
+      word.populate_list
+       expect(Word.list()).to(eq([word]))
     end
   end
-end
 
-describe('#populate_list') do
-  it('populates list with inputted term') do
-    word = Word.new({:term => 'Dog'})
-    word.populate_list
-     expect(Word.list()).to(eq([word]))
-  end
+  describe('#add_definition') do
+    it('adds new definition for inputted word') do
+      word = Word.new({:term => 'Dog', :definitions => "A soft, friendly animal"})
+      inputted_album = word.add_definition({:word => "Dog", :definitions => "A soft, friendly animal"})
+      expect(word.definitions).to(eq([inputted_album]))
+      end
+    end
 end
